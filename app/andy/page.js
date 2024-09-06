d2ffa21ef2d196fc2fe86ccb538fe98720b7f8f4
Andy Nguyen <106499410+ddtn1119@users.noreply.github.com>
2024-09-06 00:30:47 -0500

    Update page.js

--- app/andy/page.js
+++ app/andy/page.js
@@ -1,7 +1,10 @@
 export default function Page() {
     return (
         <div>
-            <h1>My name is [insert name here]</h1>
+            <h1>Hi, my name is Andy!</h1>
+            <h2>Xin chào, mình là Andy!</h2>
+            <h3>こんにちは、私の名前はアンディです！</h3>
+            <h4>안녕하세요, 제 이름은 앤디입니다!</h4>
         </div>
     );
 }
